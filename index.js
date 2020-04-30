9a37849dc108a6e60cb64155c7fa16b3a08f76fa
GroobleDierne <grooble.dierne@gmail.com>
2020-04-30 16:59:43 +0200

    Version 1.0.0

---
+++ index.js
@@ -0,0 +1,129 @@
+//Thanks to @jellz for the code I used as an inspiration
+//https://github.com/jellz/minecraft-server/blob/rewrite/src/chat/ChatUtil.js
+
+var returnExemple = {
+    text: "",
+    extra: [
+        {
+            text: "",
+            color: "",
+            bold: false,
+            italic: false,
+            underlined: false,
+            strikethrough: false,
+            obfuscated: false
+
+        }
+    ]
+}
+/**
+ * @
+ * @param {String} message the message to parse
+ * @param {boolean} acceptAndChar if set to true, the parser will consider the '&' character as a formatting character
+ * @returns {returnExemple} an object ready to be used with node-minecraft-protocol
+ */
+function parse(message, acceptAndChar = false) {
+    if (message.indexOf('§') === -1 
+    && (acceptAndChar === false || message.indexOf('&') === -1)) return { text: message }
+
+    const componentList = [];
+    let text = '';
+    let nextChanged = false;
+
+    // Default component properties
+    let color = 'reset';
+    let bold = false;
+    let italic = false;
+    let underlined = false;
+    let strikethrough = false;
+    let obfuscated = false;
+
+    function createJsonComponent() {
+        if (!text.trim()) return;
+        componentList.push({
+            text,
+            color,
+            bold,
+            italic,
+            underlined,
+            strikethrough,
+            obfuscated
+        });
+        text = '';
+    };
+
+    while (message !== '') {
+        const currentChar = message[0];
+        if (nextChanged) {
+            const newColor = colorCodes[currentChar];
+            if (newColor) {
+                if (newColor === 'bold') bold = true;
+                else if (newColor === 'strikethrough') strikethrough = true;
+                else if (newColor === 'underlined') underlined = true;
+                else if (newColor === 'italic') italic = true;
+                else if (newColor === 'obfuscated') obfuscated = true;
+                else if (newColor === '&') text += '&';
+                else if (newColor === 'reset') {
+                    strikethrough = false;
+                    bold = false;
+                    underlined = false;
+                    obfuscated = false;
+                    italic = false;
+                    color = 'reset';
+                } else color = newColor;
+            }
+            nextChanged = false;
+        } else if (currentChar === '§' || (acceptAndChar && currentChar === '&')) {
+            if (nextChanged) {
+                text += '&';
+                nextChanged = false;
+            } else {
+                nextChanged = true;
+                createJsonComponent();
+            }
+        } else {
+            text += currentChar;
+        }
+
+        message = message.slice(1, message.length);
+    }
+    createJsonComponent();
+
+    if (componentList.length > 0) {
+        return {
+            text: '',
+            extra: componentList
+        };
+    } else return { text: '' };
+
+    
+}
+
+const colorCodes = {
+    '0': 'black',
+    '1': 'dark_blue',
+    '2': 'dark_green',
+    '3': 'dark_cyan',
+    '4': 'dark_red',
+    '5': 'dark_purple',
+    '6': 'gold',
+    '7': 'gray',
+    '8': 'dark_gray',
+    '9': 'blue',
+    a: 'green',
+    b: 'aqua',
+    c: 'red',
+    d: 'light_purple',
+    e: 'yellow',
+    f: 'white',
+    k: 'obfuscated',
+    l: 'bold',
+    m: 'strikethrough',
+    n: 'underlined',
+    o: 'italic',
+    r: 'reset',
+    '&': '&',
+    '§': '&'
+}
+
+module.exports = { parse }
